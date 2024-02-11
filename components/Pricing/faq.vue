d630ef5e8cd44d981099afdbf05060018d7d7c5f
cyrusDev1 <cyrushoungue1240@gmail.com>
2024-02-11 20:14:17 +0100

    feat: mobile

--- components/Pricing/faq.vue
+++ components/Pricing/faq.vue
@@ -5,7 +5,7 @@
         </div>
         <div class="space-y-2">
             <h2 class="text-[#252B42] font-bold text-xl">the quick fox jumps over the lazy dog</h2>
-            <p class="text-md font-semibold w-[70%] text-[#737373]">Met minim Mollie non desert Alamo est sit cliquey
+            <p class="text-md font-semibold xl:w-[70%] text-[#737373]">Met minim Mollie non desert Alamo est sit cliquey
                 dolor do met sent. RELIT official consequent door ENIM
                 RELIT Mollie. Excitation venial consequent sent
                 nostrum met.</p>
